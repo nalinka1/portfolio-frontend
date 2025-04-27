aca455249bdb1fd75a396523e02117713bd69df5
Nalinka Heshan <Nalinka.Heshan@cloudsolutions.com.sa>
2025-04-27 17:45:34 +0530

    remove blog

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    eslint: {
+        ignoreDuringBuilds: true,  // Disables linting during the build
+    }
 };
 
 export default nextConfig;
